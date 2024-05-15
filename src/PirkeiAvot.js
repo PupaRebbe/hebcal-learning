60e6db2617ae7ff92901c22779c78fe60462c155
PupaRebbe <PupaRebbe@users.noreply.github.com>
2024-05-15 23:56:05 +0000

    Added PirkeiAvot

---
+++ src/PirkeiAvot.js
@@ -0,0 +1,54 @@
+var core = require("@hebcal/core");
+function getPirkeiAvot(dt, il){
+    const hd = new core.HDate(dt);
+    if(hd.getDay() < 6) throw new RangeError("Date must be Saturday");
+    const shvi = new core.HDate(21, core.months.NISAN, hd.getFullYear());
+    const diff = shvi.deltaDays(hd);
+    if(diff <0){
+        const first = shvi.after(6);
+        var weekDiff = Math.ceil(hd.deltaDays(first) / 7);
+        var chapter = 1;
+        var holObj = {}; //when a Holiday falls on Saturday, Pirkei avot is not studied
+        if(!il) {
+            holObj.Achron = shvi.next();
+            holObj.Shavuot = new core.HDate(7, core.months.SIVAN, hd.getFullYear());
+        }
+        holObj.Erev = new core.HDate(8, core.months.AV, hd.getFullYear());
+        holObj.Tb = holObj.Erev.next();
+        for (const [hol, day] of Object.entries(holObj)) {
+            if(day.deltaDays(hd) <=0){
+                if(day.isSameDate(hd)){
+                    chapter = undefined;
+                    break;
+                }
+                if(day.getDay() == 6) weekDiff -= 1;
+            }        
+        }
+        if(!chapter) return chapter;
+        if(weekDiff >= 18){ //fourth round
+            const rh = new core.HDate(1, core.months.TISHREI, hd.getFullYear() +1);
+            const last = rh.before(6);
+            var result;
+            const weeksRemain = Math.ceil(last.deltaDays(hd) / 7);
+            switch(weeksRemain){
+                case 0:
+                    result = [5, 6];
+                    break;
+                case 1:
+                    result = [3, 4];
+                    break;
+                case 2:
+                    result = (weekDiff % 6 == 1) ? [2] : [1, 2];
+                    break;
+                case 3:
+                    result = [1];
+                    break;        
+            }
+            return result;
+        }
+        chapter += weekDiff % 6;
+        return [chapter]
+    }
+}
+
+console.log(getPirkeiAvot(new Date(2023, 8, 2)));
